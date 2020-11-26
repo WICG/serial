e303980cf177738e391eea75dabfd75fc11d62e8
Dominique Hazael-Massieux <dom@w3.org>
2020-11-26 15:51:03 +0100

    Use new ReSpec profile and configuration options

--- respecConfig.js
+++ respecConfig.js
@@ -16,9 +16,7 @@ var respecConfig = {
     height: 100,
     id: 'spec-logo',
   }],
-  wg: "Web Platform Incubator Community Group",
-  // URI of the public WG page
-  wgURI: "https://www.w3.org/community/wicg/",
+  group: "wicg",
   github: "https://github.com/wicg/serial",
   xref: ["STREAMS", "DOM", "WebIDL", "HTML"]
 };
